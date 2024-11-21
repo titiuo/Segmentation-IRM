6b3f9606957cb41fb76352a465a918ae347b55cd
Titouan Duhaze <titouan.duhaze@telecom-paris.fr>
2024-11-22 00:20:50 +0100

    optimisation

---
+++ opti.py
@@ -0,0 +1,74 @@
+from library import *
+
+Dcm_patients = [str("00" + str(i)) if len(str(i)) == 1 else str("0"+str(i)) for i in range(1, 21)]
+Hcm_patients = [str("0"+str(i)) for i in range(21, 41)]
+Minf_patients = [str("0"+str(i)) for i in range(41, 61)]
+Nor_patients = [str("0"+str(i)) for i in range(61, 81)]
+Rv_patients = [str("0"+str(i)) for i in range(81, 100)]
+
+irm = Irm("001")
+img = (irm.gt1[0,:,:,1] > 2).astype(np.uint8) * 255
+def get_rayon(img):
+    # Trouver le contour de la forme
+    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Supposons qu'il n'y ait qu'une seule forme
+    cnt = contours[0]
+
+    # Trouver les moments pour calculer le centre
+    M = cv2.moments(cnt)
+    if M["m00"] != 0:
+        cx = int(M["m10"] / M["m00"])  # Coordonnée x du centre
+        cy = int(M["m01"] / M["m00"])  # Coordonnée y du centre
+    else:
+        raise ValueError("La forme est trop petite ou invalide.")
+
+    # Calculer les distances entre le centre et chaque point du contour
+    distances = [np.sqrt((x - cx) ** 2 + (y - cy) ** 2) for [[x, y]] in cnt]
+
+    # Estimer le rayon comme la moyenne des distances (ou médiane pour plus de robustesse)
+    rayon = np.mean(distances)  # ou np.median(distances)
+    print(f"Rayon estimé : {rayon:.2f} pixels")
+    return int(rayon)
+
+
+
+def get_all(irm):
+    values = {}
+    data = irm.gt1[0,:,:,:].astype(np.uint8)*255
+    N = data.shape[-1]
+    for k in range(N):
+        img = data[:,:,k]
+        try:
+            rayon = get_rayon(img)
+        except:
+            rayon = 0
+        percent = k/N
+        values[percent]=rayon
+    return values
+
+dico = {}
+
+for id in Dcm_patients:
+    print(f"Patient {id}")
+    irm=Irm(id)
+    dico[str(id)]=get_all(irm)
+for id in Hcm_patients:
+    print(f"Patient {id}")
+    irm=Irm(id)
+    dico[str(id)]=get_all(irm)
+for id in Minf_patients:
+    print(f"Patient {id}")
+    irm=Irm(id)
+    dico[str(id)]=get_all(irm)
+for id in Nor_patients:
+    print(f"Patient {id}")
+    irm=Irm(id)
+    dico[str(id)]=get_all(irm)
+for id in Rv_patients:
+    print(f"Patient {id}")
+    irm=Irm(id)  
+    dico[str(id)]=get_all(irm)
+
+with open("rayons.json", "w") as f:
+    json.dump(dico, f)
---
+++ show_opti.py
@@ -0,0 +1,96 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+Dcm_patients = [str("00" + str(i)) if len(str(i)) == 1 else str("0"+str(i)) for i in range(1, 21)]
+Hcm_patients = [str("0"+str(i)) for i in range(21, 41)]
+Minf_patients = [str("0"+str(i)) for i in range(41, 61)]
+Nor_patients = [str("0"+str(i)) for i in range(61, 81)]
+Rv_patients = [str("0"+str(i)) for i in range(81, 100)]
+#Rv_patients.append("100")
+
+with open('rayons.json', "r") as file:
+    data = json.load(file)
+
+Dcm_x = []
+Dcm_y =[]
+for id in Dcm_patients:
+    Dcm_x+=list(data[id].keys())
+    Dcm_y+=list(data[id].values())
+sorted_pairs = sorted(zip(Dcm_x, Dcm_y))  # Trie selon Dcm_x
+Dcm_x_sorted, Dcm_y_sorted = zip(*sorted(zip(Dcm_x, Dcm_y)))
+Dcm_x_sorted_float = [float(x) for x in Dcm_x_sorted]
+rounded_x = np.round(Dcm_x_sorted_float, 2)
+plt.plot(Dcm_x_sorted_float, Dcm_y_sorted, marker='o')
+plt.xticks(Dcm_x_sorted_float, labels=rounded_x, rotation=45)
+plt.xlabel("Dcm_x (arrondi à 2 décimales)")
+plt.ylabel("Dcm_y")
+plt.title("Graphique avec Dcm_x arrondi")
+plt.grid(True)
+
+Hcm_x = []
+Hcm_y =[]
+for id in Hcm_patients:
+    Hcm_x+=list(data[id].keys())
+    Hcm_y+=list(data[id].values())
+sorted_pairs = sorted(zip(Hcm_x, Hcm_y)) 
+Hcm_x_sorted, Hcm_y_sorted = zip(*sorted(zip(Hcm_x, Hcm_y)))
+Hcm_x_sorted_float = [float(x) for x in Hcm_x_sorted]
+rounded_x = np.round(Hcm_x_sorted_float, 2)
+plt.plot(Hcm_x_sorted_float, Hcm_y_sorted, marker='o')
+plt.xticks(Hcm_x_sorted_float, labels=rounded_x, rotation=45)
+plt.xlabel("Hcm_x (arrondi à 2 décimales)")
+plt.ylabel("Hcm_y")
+plt.title("Graphique avec Hcm_x arrondi")
+plt.grid(True)
+
+Minf_x = []
+Minf_y =[]
+for id in Minf_patients:
+    Minf_x+=list(data[id].keys())
+    Minf_y+=list(data[id].values())
+sorted_pairs = sorted(zip(Minf_x, Minf_y))
+Minf_x_sorted, Minf_y_sorted = zip(*sorted(zip(Minf_x, Minf_y)))
+Minf_x_sorted_float = [float(x) for x in Minf_x_sorted]
+rounded_x = np.round(Minf_x_sorted_float, 2)
+plt.plot(Minf_x_sorted_float, Minf_y_sorted, marker='o')
+plt.xticks(Minf_x_sorted_float, labels=rounded_x, rotation=45)
+plt.xlabel("Minf_x (arrondi à 2 décimales)")
+plt.ylabel("Minf_y")
+plt.title("Graphique avec Minf_x arrondi")
+plt.grid(True)
+
+Nor_x = []
+Nor_y =[]
+for id in Nor_patients:
+    Nor_x+=list(data[id].keys())
+    Nor_y+=list(data[id].values())
+sorted_pairs = sorted(zip(Nor_x, Nor_y))
+Nor_x_sorted, Nor_y_sorted = zip(*sorted(zip(Nor_x, Nor_y)))
+Nor_x_sorted_float = [float(x) for x in Nor_x_sorted]
+rounded_x = np.round(Nor_x_sorted_float, 2)
+plt.plot(Nor_x_sorted_float, Nor_y_sorted, marker='o')
+plt.xticks(Nor_x_sorted_float, labels=rounded_x, rotation=45)
+plt.xlabel("Nor_x (arrondi à 2 décimales)")
+plt.ylabel("Nor_y")
+plt.title("Graphique avec Nor_x arrondi")
+plt.grid(True)
+
+Rv_x = []
+Rv_y =[]
+for id in Rv_patients:
+    Rv_x+=list(data[id].keys())
+    Rv_y+=list(data[id].values())
+sorted_pairs = sorted(zip(Rv_x, Rv_y))
+Rv_x_sorted, Rv_y_sorted = zip(*sorted(zip(Rv_x, Rv_y)))
+Rv_x_sorted_float = [float(x) for x in Rv_x_sorted]
+rounded_x = np.round(Rv_x_sorted_float, 2)
+plt.plot(Rv_x_sorted_float, Rv_y_sorted, marker='o')
+plt.xticks(Rv_x_sorted_float, labels=rounded_x, rotation=45)
+plt.xlabel("Rv_x (arrondi à 2 décimales)")
+plt.ylabel("Rv_y")
+plt.title("Graphique avec Rv_x arrondi")
+plt.grid(True)
+
+
+
+plt.show()
